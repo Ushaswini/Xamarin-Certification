a32160007a16d1ce19a53315baaa42aebbc8f46e
Ushaswini <ushaswini.vinnakota@gmail.com>
2017-11-18 17:46:13 -0500

    NoteKeeper

---
+++ NoteKeeper_Xamarin_Forms/NoteKeeper/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NoteKeeper
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://notekeeper.azurewebsites.net";
+	}
+}
+
